0b1dccc0317dcf1cd16732e9b881dfdf32e0e2f6
vaaakoo <vaaakoo@gmail.com>
2022-11-25 18:04:15 +0400

    New #1

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        Red: "hsl(0, 100%, 74%)",
+        Green: "hsl(154, 59%, 51%)",
+        Blue: "hsl(248, 32%, 49%)",
+        DarkBlue: "hsl(249, 10%, 26%)",
+        GrayishBlue: "hsl(246, 25%, 77%)",
+      },
+      backgroundImage: {
+        desktop: "url('../images/background.jpg')",
+      },
+    },
+    fontFamily: {
+      body: ["Gentium Plus", "serif"],
+    },
+  },
+  plugins: [],
+};
